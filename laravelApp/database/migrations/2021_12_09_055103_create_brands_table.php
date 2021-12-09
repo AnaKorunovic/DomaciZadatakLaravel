d4017c234e3ae93cfb99b927f64c88c5cb559b75
AnaKorunovic <korunovicana9@gmail.com>
2021-12-09 06:52:17 +0100

    kreiranje i popunjavanje migracije Brand

---
+++ laravelApp/database/migrations/2021_12_09_055103_create_brands_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBrandsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('brands', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->timestamps();
+
+            $table->string('name', 45)->unique();
+            $table->string('slug')->unique();
+            $table->foreignId('category_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('brands');
+    }
+}
