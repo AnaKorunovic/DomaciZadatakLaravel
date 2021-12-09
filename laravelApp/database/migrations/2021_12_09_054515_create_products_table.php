ddde676a5ff190798d907bae680a2d0dc12d3c54
AnaKorunovic <korunovicana9@gmail.com>
2021-12-09 06:50:43 +0100

    kreiranje i popunjavanje migracije Product

---
+++ laravelApp/database/migrations/2021_12_09_054515_create_products_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->timestamps();
+            
+            
+            $table->text('name');  // naziv proizvoda
+            $table->text('description')->nullable();   // opis proizvod
+            $table->decimal('price', 10, 2)->nullable(); // cena
+            $table->text('slug'); // product slug
+
+            $table->foreignId('brand_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
